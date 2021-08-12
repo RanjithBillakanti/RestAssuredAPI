545f8258fa19b05e56e3957e35ab5518dc0b6a82
Naga911 <47557222+Naga911@users.noreply.github.com>
2021-08-12 18:32:59 +0530

    updated building details

---
+++ src/main/java/pojo/ContactDetails.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class ContactDetails {
+    private int id;
+    private int MobileNumber;
+
+    public int getid() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMobileNumber() {
+        return MobileNumber;
+    }
+
+    public void setMobileNumber(int MobileNumber) {
+        this.MobileNumber = MobileNumber;
+    }
+}
